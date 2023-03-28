4f96d1897187c94b3d956b0e20c51b29aa44d2e0
Leonardo Nunes <leonardonunesfava@gmail.com>
2023-03-28 19:29:16 -0300

    termino pratica 10

--- Fundamentos/secao-3/dia-1/exercicio-pratica/10/script.js
+++ Fundamentos/secao-3/dia-1/exercicio-pratica/10/script.js
@@ -3,3 +3,6 @@
 // seu valor de venda. A partir dos valores, o código deve calcular 
 // o lucro (ou seja, o valor de venda menos o custo do produto) da 
 // empresa ao vender mil produtos.
+let productCost = 30;
+let productSale = 40;
+console.log("O lucro de mil produtos foi de R$",(productSale-productCost)*1000);
