6c2e4143960b84138b33df58f1e202acb673d557
Leonardo Nunes <leonardonunesfava@gmail.com>
2023-03-28 19:25:37 -0300

    termino pratica 9

---
+++ Fundamentos/secao-3/dia-1/exercicio-pratica/9/scripts.js
@@ -0,0 +1,12 @@
+// Utilize if/else para escrever um código que defina três 
+// números em variáveis e retorne true se pelo menos uma 
+// das três for ímpar. Caso contrário, o código deve retornar false.
+let a = 2;
+let b = 4;
+let c = 8;
+if (a%2==!0 || b%2==!0 || c%2==!0) {
+    console.log(true);
+}
+else {
+    console.log(false);
+}
