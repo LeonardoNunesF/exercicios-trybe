547fd3a86d872c2580b6180e8828408e6a895897
Leonardo Nunes <leonardonunesfava@gmail.com>
2023-03-29 14:50:59 -0300

    termino exercicio 1

---
+++ Fundamentos/secao-3/dia-2/arrays.js
@@ -0,0 +1,12 @@
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1]; //Obtenha o valor “Serviços” do array
+
+console.log(menuServices);
+
+let indexOfPortfolio = menu.indexOf("Portfólio"); //Procure o índice do valor “Portfólio” do array
+
+console.log(indexOfPortfolio);
+
+console.log(menu); //Adicione o valor “Contato” no final do array 
+
+menu.push('Contato');
