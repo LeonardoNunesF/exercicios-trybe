751aea1f35b6adffc6706dc443e6a08e61278d80
Leonardo Nunes <leonardonunesfava@gmail.com>
2023-03-29 15:13:32 -0300

    termino exercicio 3

---
+++ Fundamentos/secao-3/dia-2/for-of.js
@@ -0,0 +1,4 @@
+let names = ['João', 'Maria', 'Antônio', 'Margarida']; //Utilize o for/of para imprimir os elementos da lista names com o console.log():
+for (let index of names) {
+    console.log(index);
+}
