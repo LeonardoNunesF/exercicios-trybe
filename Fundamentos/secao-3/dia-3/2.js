5a126dcacb6d82aa1ddee7876976d553188c8e50
Leonardo Nunes <leonardonunesfava@gmail.com>
2023-04-03 15:25:40 -0300

    termino exercicio parametros

--- Fundamentos/secao-3/dia-3/2.js
+++ Fundamentos/secao-3/dia-3/2.js
@@ -19,3 +19,36 @@ console.log(clientAdd(45));
 console.log(clientAdd('Leonardo')); 
 console.log(clientesTrybeBank); 
 
+// Agora iremos escrever um programa que irá excluir um cliente
+// já existente do array de clientes do TrybeBank. Certifique-se
+// de que a função deve receber um parâmetro do tipo string e 
+// imprimir uma mensagem de erro, caso o parâmetro não seja do 
+// tipo string e caso o cliente não exista dentro do array. 
+
+clientesTrybeBank = ['Ada', 'John', 'Gus'];
+
+// escreva aqui sua função
+function clientRemove(client) {
+    if (typeof client === 'string') {
+        let clientFound = false;
+        for (let i = 0; i < clientesTrybeBank.length; i++) {  
+            if (clientesTrybeBank[i] === client) {
+                clientesTrybeBank.splice(i, 1);
+                clientFound = true;
+                return 'Cliente excluido com sucesso!';
+            }
+        } 
+        if (clientFound === false){
+            return 'Cliente não encontrado.'
+        }
+    }
+    else {
+        return 'Parâmetro inválido.'
+    }
+    
+}
+
+console.log(clientRemove(45)); 
+console.log(clientRemove('Leonardo')); 
+console.log(clientRemove('Gus'));
+console.log(clientesTrybeBank); 
