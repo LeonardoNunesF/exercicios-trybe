59297f8d6be0d8e281920056f6210b450b7fd6e2
Leonardo Nunes <leonardonunesfava@gmail.com>
2023-04-04 12:31:49 -0300

    inicio exercicio bonus

---
+++ Fundamentos/secao-3/dia-4/exercicioBonus.js
@@ -0,0 +1,25 @@
+// Suponha que você esteja trabalhando em uma escola e precise 
+// fazer algumas atualizações no sistema. Para isso, considere o seguinte código:
+let lesson1 = {
+    materia: 'Matemática',
+    numeroEstudantes: 20,
+    professor: 'Maria Clara',
+    turno: 'manhã',
+  };
+  
+  let lesson2 = {
+    materia: 'História',
+    numeroEstudantes: 20,
+    professor: 'Carlos',
+  };
+  
+  let lesson3 = {
+    materia: 'Matemática',
+    numeroEstudantes: 10,
+    professor: 'Maria Clara',
+    turno: 'noite',
+  };
+
+//   Crie uma função para adicionar o turno da noite na lesson2. Essa função deve 
+//   ter três parâmetros: o objeto a ser modificado, a chave a ser adicionada 
+//   e o valor dela. 
