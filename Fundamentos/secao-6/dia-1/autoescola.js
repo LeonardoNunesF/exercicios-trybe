86e5e43be0b1b7bc9398d2a30645a5808d82d029
Leonardo Nunes <leonardonunesfava@gmail.com>
2023-04-27 19:22:28 -0300

    termino exercicios mais ou menos

--- Fundamentos/secao-6/dia-1/autoescola.js
+++ Fundamentos/secao-6/dia-1/autoescola.js
@@ -7,10 +7,26 @@ const verifyStringNumb = (name, age) => {
   }
 };
 
+const verifyAge = (age) => {
+  if (age < 18) {
+    throw new Error(
+      "Ops, infelizmente nesse momento você não pode fazer as aulas"
+    );
+  }
+};
+
 const studentRegister = (name, age) => {
   try {
     verifyStringNumb(name, age);
+    verifyAge(age);
+    return `${name}, seja bem-vindo(a) à AuTrybe!`;
   } catch (error) {
     return error.message;
   }
 };
+
+console.log(studentRegister('Zeca', 23));
+console.log(studentRegister('', 23));
+console.log(studentRegister('Zeca'));
+console.log(studentRegister(23, 'Zeca'));
+console.log(studentRegister('Zecalo', 17));
