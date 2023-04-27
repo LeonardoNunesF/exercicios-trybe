71f327037a8377fd954a5b6009d06da59208d491
Leonardo Nunes <leonardonunesfava@gmail.com>
2023-04-27 15:59:04 -0300

    feito exemplo 2

--- Fundamentos/secao-6/script.js
+++ Fundamentos/secao-6/script.js
@@ -1,17 +1,17 @@
 // Primeira parte
 const promo = [
-    {product: 'bola de beach tennis', price: 29.99},
-    {product: 'mochila de trilha 20 litros', price: 120.99},
-    {product: 'capacete de ciclista', price: 180.59},
-    {product: 'toalha compacta de secagem', price: 39.99},
-    {product: 'kit 5 camisetas Dry Fit', price: 99.99},
-    {product: 'caneleiras de peso - 3kg (par)', price: 44.99},
-    {product: 'bola de ginástica e pilates', price: 149.59},
-    {product: 'óculos de natação', price: 18.99},
-    {product: 'capa protetora de prancha de surf', price: 89.59},
-    {product: 'bola de basquete', price: 39.99},
-  ];
-  const button = document.querySelector('#send-button');
+  {product: 'bola de beach tennis', price: 29.99},
+  {product: 'mochila de trilha 20 litros', price: 120.99},
+  {product: 'capacete de ciclista', price: 180.59},
+  {product: 'toalha compacta de secagem', price: 39.99},
+  {product: 'kit 5 camisetas Dry Fit', price: 99.99},
+  {product: 'caneleiras de peso - 3kg (par)', price: 44.99},
+  {product: 'bola de ginástica e pilates', price: 149.59},
+  {product: 'óculos de natação', price: 18.99},
+  {product: 'capa protetora de prancha de surf', price: 89.59},
+  {product: 'bola de basquete', price: 39.99},
+];
+const button = document.querySelector('#send-button');
 
 button.addEventListener('click', () => {
   const name = document.querySelector('#name-id').value;
@@ -23,14 +23,41 @@ button.addEventListener('click', () => {
 const showPromo = (name, number) => {
   const firstText = document.querySelector('#text-initial');
   const secondText = document.querySelector('#text-final');
-  const productObject = checkNumber(parseInt(number));
+  
+  try {
+    checkName(name);
+    checkNumber(parseInt(number));
+    const productObject = checkPromo(number);
 
-  firstText.innerHTML = `Boas-vindas, ${name}!`;
-  secondText.innerHTML = `A promoção do dia é: 
-    ${productObject.product} no valor de R$ ${productObject.price}`;
+    firstText.innerHTML = `Boas-vindas, ${name}!`;
+    secondText.innerHTML = `A promoção do dia é: 
+      ${productObject.product} no valor de R$ ${productObject.price}`;
+  } catch(err) {
+    secondText.innerHTML = err.message;
+  }
+}
+const checkName = (name) => {
+  let letters = /[aA-zZ]+/;
+  
+  if (!name.match(letters)) {
+    throw new Error('É necessário digitar um nome válido');
+  }
 }
 
 // Segunda parte
 const checkNumber = (number) => {
-  return promo[number - 1];
+  if (isNaN(number)) {
+    throw new Error('É necessário digitar um número');
+  }
+}
+
+// Terceira parte
+const checkPromo = (number) => {
+  for (let index = 0; index < promo.length; index += 1) {
+    if (number - 1 === index) {
+      return promo[index];
+    } else {
+      throw new Error ('É necessario digitar um número de 1 a 10');
+    }
+  }
 }
