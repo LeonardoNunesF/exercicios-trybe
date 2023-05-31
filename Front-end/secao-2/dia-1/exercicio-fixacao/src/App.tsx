f4a16b7a68d9311a48f18d947988c28ad93c9cfe
Leonardo Nunes <leonardonunesfava@gmail.com>
2023-05-31 12:36:42 -0300

    termino conteudo dia 1 secao 2

--- Front-end/secao-2/dia-1/exercicio-fixacao/src/App.tsx
+++ Front-end/secao-2/dia-1/exercicio-fixacao/src/App.tsx
@@ -1,9 +1,15 @@
-import Header from './Header';
+import Greeting from "./Greeting";
+import LuckyNumbers from "./LuckyNumbers";
 
 import './App.css';
 
 function App() {
-  return <Header />;
+  return (
+    <>
+    <Greeting />
+    <LuckyNumbers />
+  </>
+  );
 }
 
 export default App;
